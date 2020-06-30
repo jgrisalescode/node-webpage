473097630453f6809b5b43112d0f4154a7ab7576
JuliaGrisales <jgrisales@gmail.com>
2020-06-30 16:33:00 +0200

    Handelbars

--- server.js
+++ server.js
@@ -4,19 +4,14 @@ const app = express()
 // Giving static content
 app.use(express.static(__dirname + "/public"))
 
-app.get("/", (req, res) => {
-  res.send("Hello World!")
-})
+// Express HBS engine
+app.set("view engine", "hbs")
 
-app.get("/data", (req, res) => {
-  // Giving a JSON whit Express, simple and fast
-  let exit = {
+app.get("/", (req, res) => {
+  res.render("home", {
     name: "Julián",
-    lastName: "Grisales",
-    age: 40,
-    url: req.url
-  }
-  res.send(exit) // Express makes all the work whit JSON parsing, see app-old to compare
+    year: new Date().getFullYear()
+  })
 })
 
 app.listen(3000, () => {
