ba10a7a97de4a974b24e2938649091fc82c8df53
JuliaGrisales <jgrisales@gmail.com>
2020-06-30 15:44:12 +0200

    Introducing Express

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+})
+
+app.get("/data", (req, res) => {
+  // Giving a JSON whit Express, simple and fast
+  let exit = {
+    name: "Julián",
+    lastName: "Grisales",
+    age: 40,
+    url: req.url
+  }
+  res.send(exit) // Express makes all the work whit JSON parsing, see app-old to compare
+})
+
+app.listen(3000, () => {
+  console.log("Listening requests on port 3000")
+})
