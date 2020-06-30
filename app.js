e9e132fad45e39ff19873c1078813fa803514f5b
JuliaGrisales <jgrisales@gmail.com>
2020-06-30 15:25:12 +0200

    Creating a basic webserver whit JSON like response

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const http = require("http")
 // Creating a simple webserver
 http
   .createServer((req, res) => {
-    res.write("Hello World")
+    res.writeHead(200, { "Content-Type": "application/json" })
+    let exit = {
+      name: "Julián",
+      lastName: "Grisales",
+      age: 40,
+      url: req.url
+    }
+    res.write(JSON.stringify(exit))
+    // res.write("Hello World")
     res.end()
   })
   .listen(3000)
